a259d118c7cf3dcda5d25594f351aeef31768996
Dev1nshu <devanshumali53@gmail.com>
2026-04-28 10:49:33 +0530

    Base application setup

---
+++ test/com/apps/quantitymeasurement/QuantityMeasurementAppTest.java
@@ -0,0 +1,34 @@
+package com.apps.quantitymeasurement;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import com.apps.quantitymeasurement.QuantityMeasurementApp.Feet;
+
+public class QuantityMeasurementAppTest {
+
+    @Test
+    public void testFeetEquality_SameValue() {
+        Feet feet1 = new Feet(1.0);
+        Feet feet2 = new Feet(1.0);
+        assertEquals(feet1, feet2);
+    }
+
+    @Test
+    public void testFeetEquality_DifferentValue() {
+        Feet feet1 = new Feet(1.0);
+        Feet feet2 = new Feet(2.0);
+        assertNotEquals(feet1, feet2);
+    }
+
+    @Test
+    public void testFeetEquality_NullComparison() {
+        Feet feet1 = new Feet(1.0);
+        assertFalse(feet1.equals(null));
+    }
+
+    @Test
+    public void testFeetEquality_SameReference() {
+        Feet feet1 = new Feet(1.0);
+        assertEquals(feet1, feet1);
+    }
+}
